5e76773011d0556462f53b4b9f43b3bb1c1152f3
Ian Bond <bondninian@icloud.com>
2024-07-31 20:54:22 -0400

    multiMap test passing

--- problems/03-multi-map.js
+++ problems/03-multi-map.js
@@ -21,10 +21,30 @@ let result3 = multiMap("hi", 5, function(s) {
 console.log(result3); // hi!!!!!
 *******************************************************************************/
 
-let multiMap = function() {
-
+let multiMap = function(val, n, cb) {
+  let result = cb(val);
+  for (let i = 1; i < n; i++) {
+    result = cb(result);
+  }
+  return result; 
 };
 
+
+
+let result1 = multiMap(7, 2, function(n) {
+  return n * 10;
+});
+console.log(result1); // 700
+
+let result2 = multiMap(7, 3, function(n) {
+    return n * 10;
+});
+console.log(result2); // 7000
+
+let result3 = multiMap("hi", 5, function(s) {
+  return s + "!";
+});
+console.log(result3); // hi!!!!!
 
 
 
