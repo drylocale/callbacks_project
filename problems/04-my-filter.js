88736c6709a0784079a656f3300b2c7f269a31a1
Ian Bond <bondninian@icloud.com>
2024-07-31 21:00:05 -0400

    myFilter passing all tests

--- problems/04-my-filter.js
+++ problems/04-my-filter.js
@@ -19,8 +19,15 @@ let result2 = myFilter(['choose', 'big', 'words', 'only'], function (s) {
 console.log(result2);      // ['choose', 'words', 'only']
 *******************************************************************************/
 
-let myFilter = function() {
+let myFilter = function(array, cb) {
+    let result = [];
+    for (let i = 0; i < array.length; i++) {
+        if (cb(array[i])) {
+            result.push(array[i])
+        }
+    }
 
+    return result; 
 };
 
 
