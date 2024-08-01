35483f3cca6c4cf508bebd8cde6ce4bd0eac9071
Ian Bond <bondninian@icloud.com>
2024-07-31 20:32:33 -0400

    myMap passing all tests

--- problems/02-my-map.js
+++ problems/02-my-map.js
@@ -16,9 +16,21 @@ let result2 = myMap(['run', 'Forrest'], function (el) {
 console.log(result2);   // [ 'RUN!', 'FORREST!' ]
 *******************************************************************************/
 
-let myMap = function() {
-
+let myMap = function(array, cb) {
+    let result = []; 
+    for (let i = 0; i < array.length; i++) {
+        result.push(cb(array[i], i, array));
+    }
+    return result; 
 };
+
+let result1 = myMap([100, 25, 81, 64], Math.sqrt);
+console.log(result1);   // [ 10, 5, 9, 8 ]
+
+let result2 = myMap(['run', 'Forrest'], function (el) {
+    return el.toUpperCase() + '!';
+});
+console.log(result2);   // [ 'RUN!', 'FORREST!' ]
 
 
 
