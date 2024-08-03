865f0c2433a9451cb01b69005bf3f5292b1168e2
Ian Bond <bondninian@icloud.com>
2024-08-03 10:55:40 -0400

    alternatingMap passing all tests

--- problems/23-alternating-map.js
+++ problems/23-alternating-map.js
@@ -36,8 +36,18 @@ console.log(alternatingMap(['hEy', 'EVERYone', 'whats', 'uP??'], yell, whisper))
 *******************************************************************************/
 
 
-let alternatingMap = function() {
+let alternatingMap = function(array, cb1, cb2) {
+    let result = []; 
+    for (let i = 0; i < array.length; i++) {
+        let ele = array[i];
+        if (i % 2 === 0) {
+            result.push(cb1(ele));
+        } else {
+            result.push(cb2(ele));
+        }
+    }
 
+    return result; 
 };
 
 
